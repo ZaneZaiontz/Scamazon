5624e0bc07b347920895b1256dcd3dbc40a7aef1
Eric Sananikone <sananikoneeric1@gmail.com>
2022-03-15 18:04:44 -0500

    The driver file

---
+++ app.js
@@ -0,0 +1,97 @@
+/* Tools Requirements */
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user_model');
+
+/* Routes requirements */
+const productRoutes = require('./routes/product_route');
+const userRoutes = require('./routes/user_route');
+
+/* Error Handler requirements */
+const ExpressError = require('./utils/ExpressError');
+
+/* Connect to mongDB */
+mongoose.connect('mongodb://localhost:27017/scamazon-demo', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+//Check if connection is successful
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+	console.log('Database Connected');
+});
+
+const app = express();
+
+//set engine
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+const sessionConfig = {
+	secret: 'thisshouldbeabettersecret!',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7
+	}
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//define middleware for flash
+app.use((req, res, next) => {
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
+});
+
+//define route handlers
+app.use('/', userRoutes);
+app.use('/products', productRoutes);
+
+/* #### BEGIN ROUTE DEFINITIONS #### */
+//home page route
+app.get('/', (req, res) => {
+	res.render('home');
+});
+/* #### END ROUTE DEFINITIONS #### */
+
+/* Begin Error Handlers */
+app.all('*', (req, res, next) => {
+	next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+	res.status(statusCode).render('error', { err });
+});
+/* End Error Handlers */
+
+//port the app is listening on
+app.listen(3000, () => {
+	console.log('APP IS LISTENING ON PORT 3000!');
+});
