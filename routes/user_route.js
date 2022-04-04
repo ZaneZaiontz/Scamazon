f8d63a133ac8af33e4b934d352a849ba458b15aa
Caleb Kopecky <calebjkopecky@gmail.com>
2022-04-03 20:13:17 -0500

    added requirements for usernames when registering

--- routes/user_route.js
+++ routes/user_route.js
@@ -5,6 +5,9 @@ const User = require('../models/user_model');
 /* 3/29 - added the following requirements */
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
+
+
+
 //register route: Gets register form
 router.get('/register', (req, res) => {
 	res.render('users/register');
@@ -16,6 +19,15 @@ router.post(
 	catchAsync(async (req, res, next) => {
 		try {
 			const { username, password } = req.body;
+
+			// username must be between 5 and 15 characters
+			// and only contain letters
+			if (! username.match("^[a-zA-z]{5,15}$")){
+				req.flash('error', 'Invalid Username');
+				res.redirect('/register');
+				return;
+			}
+
 			const user = new User({ username });
 			/* 3/29 */
 			//Check if the admin code field matches the admin code
@@ -45,24 +57,24 @@ router.get('/login', (req, res) => {
 //post for login route
 /* 3/29 */
 /* Verify's the credentials.
-	- if correct - will flash a success message and redirect to products page
+	- if correct - will flash a success message and redirect to profile page
 	- if incorrect - will flash an error message and redirect to login page \
 */
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
 	req.flash('success', 'Welcome back!');
 	/* 3/30 - once logged in, either redirect user to the url they're requesting or to products page */
-	const redirectUrl = req.session.returnTo || '/products';
+	const redirectUrl = req.session.returnTo || '/profile';
 	//delete redirctURL from the session
 	delete req.session.returnTo;
 	res.redirect(redirectUrl);
 });
 
-//Logic for logging out. Will redirect user to products page once logged out
+//Logic for logging out. Will redirect user to home page once logged out
 /* 3/30 - added logout route */
 router.get('/logout', (req, res) => {
 	req.logout();
 	req.flash('success', 'Logged out.');
-	res.redirect('/products');
+	res.redirect('/');
 });
 
 router.get('/profile', (req, res) => {
