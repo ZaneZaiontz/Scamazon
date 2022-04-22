fdc0625075c12657ed185fb560cb483fb4244d8f
Zane <zanezaiontz@live.com>
2022-04-22 18:53:58 -0500

    Added back some commented out code

--- routes/product_route.js
+++ routes/product_route.js
@@ -23,6 +23,23 @@ router.get(
 	})
 );
 
+
+//products page route: will display all products
+// router.get(
+// 	'/',
+// 	catchAsync(async (req, res) => {
+// 		var sort = req.query.sort;
+// 		var sortOrder = 
+// 			sort === "1" ? {price: -1}
+// 			: sort === "2" ? {price: 1}
+// 			: sort === "3" ? {quantity: -1}
+// 			: sort === "4" ? {quantity: 1}
+// 			: {};
+// 		const products = await Product.find({}).sort(sortOrder);
+// 		res.render('products/index', { products });
+// 	})
+// );
+
 //search products route: will display the product as customer types in 
 
 
